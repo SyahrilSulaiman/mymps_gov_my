e3425c46f07436c090bb87134ac4f876b0da1844
wanasyraf96 <wanasyraf96@yahoo.com>
2020-11-11 00:31:12 +0800

    Bill from db

--- src/BayarBill.js
+++ src/BayarBill.js
@@ -13,6 +13,7 @@ import { ResponsiveEmbed } from "react-bootstrap";
 import { BrowserRouter as Router,Switch, Route, Link, useParams, useRouteMatch } from "react-router-dom";
 import SenaraiBil from './SenaraiBil';
 import Carian from './Carian';
+import BillList from './BillList';
 
 function Bill(props) {
 
@@ -131,85 +132,9 @@ function Bill(props) {
 								// View bill state -> class jugak
 								// routing sikit
 							}
-							<div className="px-4 md:px-2 mx-auto w-full" onClick={handleViewBill}>
-								<div className="flex flex-wrap">
-									<div className="w-full px-4">
-										<div className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 shadow-lg">
-											<div className="flex-auto p-4">
-												<div className="flex flex-row pt-4">
-													<div className="relative w-full pr-4 max-w-full flex-grow flex-1">
-														<span className="font-semibold text-lg text-gray-800">
-														{dataset.jenis}
-														</span>	
-													</div>
-													<div className="relative w-auto pl-4 flex-initial">
-														<span className="font-semibold text-lg text-gray-800">
-														{dataset.amaun}
-														</span>	
-													</div>
-												</div>
-												<div className="flex flex-row pb-4">
-													<div className="relative w-full pr-4 max-w-full flex-grow flex-1">
-														<span className="font-semibold text-lg text-gray-800">
-														{dataset.akaun}
-														</span>	
-														<h5 className="uppercase font-medium text-xs text-gray-600">
-														{dataset.tempoh}
-														</h5>
-													</div>
-													<div className="relative w-auto pl-4 flex-initial">
-														<h5 className="uppercase font-medium text-xs text-green-600">
-														{dataset.status}
-														</h5>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="px-4 md:px-2 mx-auto w-full" onClick={handleBayar}>
-								<div className="flex flex-wrap">
-									<div className="w-full px-4">
-										<div className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 shadow-lg">
-											<div className="flex-auto p-4">
-												<div className="flex flex-row pt-4">
-													<div className="relative w-full pr-4 max-w-full flex-grow flex-1">
-														<span className="font-semibold text-lg text-gray-800">
-														Cukai Taksiran
-														</span>	
-													</div>
-													<div className="relative w-auto pl-4 flex-initial">
-														<span className="font-semibold text-lg text-gray-800">
-														RM 40.00
-														</span>	
-													</div>
-												</div>
-												<div className="flex flex-row pb-4">
-													<div className="relative w-full pr-4 max-w-full flex-grow flex-1">
-														<span className="font-semibold text-lg text-gray-800">
-														A929739
-														</span>	
-														<h5 className="uppercase font-medium text-xs text-gray-600">
-														Julai - disember 2020
-														</h5>
-													</div>
-													<div className="relative w-auto pl-4 flex-initial">
-														<h5 className="uppercase font-medium text-xs text-red-600">
-														Belum Dibayar
-														</h5>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
+							<BillList />
 					</div>
 			</div>
-				
-				<Carian />
-				<SenaraiBil />
 
         <Footer />
       </div>
--- src/BillList.js
+++ src/BillList.js
@@ -1,215 +1,107 @@
-import React, { useState, useEffect } from "react";
-import DataTable from "react-data-table-component";
-import Card from "@material-ui/core/Card";
-import { getUser, getNOKP, getToken, removeUserSession } from "./Utils/Common";
-import MainDashboard from "./views/admin/Dashboard";
-import Sidebar from "./Sidebar";
-import Navbar from "./components/Navbars/AdminNavbar";
-import Information from "./components/Cards/CardSettings";
-import Footer from "./components/Footers/Footer";
-import { data } from "jquery";
-import swal from "sweetalert";
-import { ResponsiveEmbed } from "react-bootstrap";
-import { BrowserRouter as Router,Switch, Route, Link, useParams, useRouteMatch } from "react-router-dom";
-import SenaraiBil from './SenaraiBil';
-import Carian from "./Carian";
+import React, {Component, useState, useEffect} from 'react';
+import axios from 'axios';
 
-function BillList(props) {
+export default function BillList(){
 
-	const token   = getToken();
-	const user    = getUser();
-	const nokp    = getNOKP();
-
-	const [include,isInclude] = useState(false);
-	const [dataset, setDataSet] = useState({
-		jenis: 'Cukai Taksiran',
-		akaun: 'A929181',
-		amaun:'RM 30.00',
-		tempoh:'Januari - Jun 2020',
-		status:'Telah dibayar'
-	});
-
-		// setDataset({
-		// 	...dataset,
-		// 	jenis: 'Cukai Taksiran',
-		// 	akaun: 'A929181',
-		// 	amaun:'RM 30.00',
-		// 	tempoh:'Julai - Disember 2020',
-		// 	status:'Telah dibayar'
-		// });
-
-  // useEffect(async() => {
-
-  //   var url = "https://toyyibpay.com/api/getBankFPX";
-
-  //   const tarik   = await fetch(url);
-  //   const data    = await tarik.json();
-
-  //   for(var i = 0; i < data.length; i++){
-  //     const item  = data[i].CODE;
-  //     setDataSet(item);
-  //   }
-    
-	// }, []);
-
-	// bil > {bawak state} > bil detail
-	
-	
-	const handleReceipt= (e) =>{
-		// let url = 
-		console.log(user);
-		console.log(token);
-		console.log(nokp);
-		let id= '1';
-
-		let url = btoa('nokp='+nokp+'&code=receipt'+'&id='+id);
-		window.open('https://mymps.corrad.my/rp/receipt.php?'+url);
-	}
-
-	const handleReceipt2 = (e) => {
-		let id= '2';
-		let url = btoa('nokp='+nokp+'&code=receipt'+'&id='+id);
-		window.open('https://mymps.corrad.my/rp/receipt.php?'+url);
-  }
-  
-  const handleViewBill = (e) => {
+    const handleViewBill = (e) => {
 		// set=[e.target.id]
+		// return (
+		// 	 senarai = <Senarai />
 		console.log('View');
-		window.location.href = "/senaraibill";
-	}
-	
-	const handleAddBill = (e) => {
-		// set=[e.target.id]
-		console.log('Add Bill');
-  }
+        window.location.href = "/senaraibill";
+        
+    }
+    
+    const handleBayar = (e) => {
+        console.log('Bayar');
+        window.location.href = '/payment';
+    }
+    
+    const [dataset, setDataSet] = useState({
+        datas: []
+    });
 
-  const handleBayar = () => {
-	  console.log('Bayar');
-	  window.location.href = '/payment';
-  }
-  
+    
+    useEffect (() => {
+        axios.get('https://mymps.corrad.my/int/api_generator.php?api_name=getBill')
+        .then(res => {
+            console.log(res.data.data)
+            setDataSet({
+                datas:res.data.data
+                })
+            }
+        )
 
-  return (
-	<div>
+    },[])
 
-				<div className="relative bg-gray-600 md:pt-32 pt-4 pb-4" style={{height: "90vh"}}>
+    const bills = dataset.datas.length ? (
+        dataset.datas.map(bill => {
+            return (
+                <div className="px-4 md:px-2 mx-auto w-full"
+                 onClick={
+                     //condition xbetul
+                     (bill.status.toUpperCase() == 'TERTUNGGAK') ? (handleBayar):(handleViewBill)
+                    }
+                key = {bill.id}>
+                    <div className="flex flex-wrap">
+                        <div className="w-full px-4">
+                            <div className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 shadow-lg">
+                                <div className="flex-auto p-4">
+                                    <div className="flex flex-row pt-4">
+                                        <div className="relative w-full pr-4 max-w-full flex-grow flex-1">
+                                            <span className="font-semibold text-lg text-gray-800">
+                                            {
+                                            // dataset.jenis
+                                            bill.type
+                                            }
+                                            </span>	
+                                        </div>
+                                        <div className="relative w-auto pl-4 flex-initial">
+                                            <span className="font-semibold text-lg text-gray-800">
+                                            {
+                                                //dataset.amaun
+                                            }
+                                            </span>	
+                                        </div>
+                                    </div>
+                                    <div className="flex flex-row pb-4">
+                                        <div className="relative w-full pr-4 max-w-full flex-grow flex-1">
+                                            <span className="font-semibold text-lg text-gray-800">
+                                            {
+                                            // dataset.akaun
+                                            bill.code
+                                            }
+                                            </span>	
+                                            <h5 className="uppercase font-medium text-xs text-gray-600">
+                                            {
+                                            // dataset.tempoh
+                                            bill.description
+                                            }
+                                            </h5>
+                                        </div>
+                                        <div className="relative w-auto pl-4 flex-initial">
+                                            <h5 className="uppercase font-medium text-xs text-green-600">
+                                            {
+                                            // dataset.status
+                                            bill.status
+                                            }
+                                            </h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+        })
+    ) : (
+        <div></div>
+    )
 
-
-					<div className="px-4 md:px-10 mx-auto w-full" >
-						<div className="flex flex-wrap">
-							<div className="w-full px-4">
-								<div className="relative flex flex-col min-w-0 break-words">
-									<div className="flex-auto p-4">
-										<div className="flex flex-row ">
-											<div className="relative w-full pr-4 max-w-full flex-grow flex-1">
-												<span className="font-semibold text-xl text-white">
-												Bil Saya
-												</span>	
-											</div>
-											{
-												//  Function Add bill
-												//  function xhold apa2 state boleh terus jadi function
-												//  create function
-												// form input?
-											}
-											<div className="relative w-auto pl-4 flex-initial" onClick={handleAddBill}>
-												<span className="font-semibold text-lg text-white">
-													+ Tambah
-												</span>	
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					{
-						// List state -> so class
-						// View bill state -> class jugak
-						// routing sikit
-					}
-					<div className="px-4 md:px-2 mx-auto w-full" onClick={handleViewBill}>
-						<div className="flex flex-wrap">
-							<div className="w-full px-4">
-								<div className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 shadow-lg">
-									<div className="flex-auto p-4">
-										<div className="flex flex-row pt-4">
-											<div className="relative w-full pr-4 max-w-full flex-grow flex-1">
-												<span className="font-semibold text-lg text-gray-800">
-												{dataset.jenis}
-												</span>	
-											</div>
-											<div className="relative w-auto pl-4 flex-initial">
-												<span className="font-semibold text-lg text-gray-800">
-												{dataset.amaun}
-												</span>	
-											</div>
-										</div>
-										<div className="flex flex-row pb-4">
-											<div className="relative w-full pr-4 max-w-full flex-grow flex-1">
-												<span className="font-semibold text-lg text-gray-800">
-												{dataset.akaun}
-												</span>	
-												<h5 className="uppercase font-medium text-xs text-gray-600">
-												{dataset.tempoh}
-												</h5>
-											</div>
-											<div className="relative w-auto pl-4 flex-initial">
-												<h5 className="uppercase font-medium text-xs text-green-600">
-												{dataset.status}
-												</h5>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-
-				<div className="px-4 md:px-2 mx-auto w-full" onClick={handleBayar}>
-					<div className="flex flex-wrap">
-						<div className="w-full px-4">
-							<div className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 shadow-lg">
-								<div className="flex-auto p-4">
-									<div className="flex flex-row pt-4">
-										<div className="relative w-full pr-4 max-w-full flex-grow flex-1">
-											<span className="font-semibold text-lg text-gray-800">
-											Cukai Taksiran
-											</span>	
-										</div>
-										<div className="relative w-auto pl-4 flex-initial">
-											<span className="font-semibold text-lg text-gray-800">
-											RM 40.00
-											</span>	
-										</div>
-									</div>
-									<div className="flex flex-row pb-4">
-										<div className="relative w-full pr-4 max-w-full flex-grow flex-1">
-											<span className="font-semibold text-lg text-gray-800">
-											A929739
-											</span>	
-											<h5 className="uppercase font-medium text-xs text-gray-600">
-											Julai - disember 2020
-											</h5>
-										</div>
-										<div className="relative w-auto pl-4 flex-initial">
-											<h5 className="uppercase font-medium text-xs text-red-600">
-											Belum Dibayar
-											</h5>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				
-			</div>
-
-		
-        <Footer />
-    </div>
-  );
+    return (
+        <div className="">
+            {bills}
+        </div>
+    )
 }
-
-export default BillList;
