61784bdc49e2a5b27322615aab3008b5c9d6731d
wanasyraf96 <wanasyraf96@yahoo.com>
2020-11-12 17:16:31 +0800

    Add Bil
    Function add bil

--- src/Add.js
+++ src/Add.js
@@ -4,52 +4,13 @@ import Sidebar from "./Sidebar";
 import Navbar from "./components/Navbars/AdminNavbar";
 import axios from "axios";
 import swal from 'sweetalert';
+import Carian from './Carian';
+import Search from './Search';
 
-export default function Carian({props}){
-    const [search,setSearch] = useState({type:'',search:''});
-
-    const handleSearchOption = (e) => {
-        setSearch({
-            ...search,
-            [e.target.id]: e.target.value
-        });
-    }
-
-    let id = useFormInput("");
-
-    const handleSearch = (e) => {
-        e.preventDefault();
-        // + something something something
-        let formdata = new FormData();
-        formdata.append('id',id.value)
-        axios.post('https://mymps.corrad.my/int/api_generator.php?api_name=newBill',formdata)
-        .then(response=>{
-
-            if(response.status == "success"){
-                console.log(response.status)
-                swal('Berjaya Tambah','Berjaya tambah akaun untuk pembayaran','success')
-                window.location.href = '/bill';
-            }
-            else{
-                swal('Tidak Berjaya','No Akaun tidak dijumpai','error')
-            }
-        })
-        // .catch(err => {errors: err.response});
-
-        // ada bill masuk bill list xde error
-    }
-
-    // const [dataset, setDataSet] = useState({
-	// 	jenis: 'Cukai Taksiran',
-	// 	akaun: 'A245181',
-	// 	amaun:'RM 30.00',
-	// 	tempoh:'Januari - Jun 2020',
-	// 	status:'Belum dibayar'
-    // });
+export default function Add({props}){
+    const [type,setType] = useState('');
 
 
-    if(search.type == '')
-    {
         return (
             <div>
             <Sidebar />
@@ -64,19 +25,19 @@ export default function Carian({props}){
                                     <div className="flex-auto p-4">
 
                                     <div className="flex flex-initial flex-row-reverse pt-4 pb-4">
-                                        <button onClick={(e) => handleSearchOption(e)} id="type" value="nokp" className="text-black bg-white text-center flex-row-reverse border-solid border-2 border-gray-300 rounded-md w-full h-12">
+                                        <button onClick={() => setType('nokp')} className="text-black bg-white text-center flex-row-reverse border-solid border-2 border-gray-300 rounded-md w-full h-12">
                                                     No Kad Pengenalan
                                         </button>
                                     </div>
 
                                     <div className="flex flex-initial flex-row-reverse pt-4 pb-4">
-                                        <button onClick={(e) => handleSearchOption(e)} id="type" value="ssm" className="text-black bg-white text-center flex-row-reverse border-solid border-2 border-gray-300 rounded-md w-full h-12">
+                                        <button onClick={() => setType('ssm')} className="text-black bg-white text-center flex-row-reverse border-solid border-2 border-gray-300 rounded-md w-full h-12">
                                                     No SSM Pendaftaran Syarikat
                                         </button>
                                     </div>
 
                                     <div className="flex flex-initial flex-row-reverse pt-4 pb-4">
-                                        <button onClick={(e) => handleSearchOption(e)} id="type" value="akaun" className="text-black bg-white text-center flex-row-reverse border-solid border-2 border-gray-300 rounded-md w-full h-12">
+                                        <button onClick={() => setType('akaun')}className="text-black bg-white text-center flex-row-reverse border-solid border-2 border-gray-300 rounded-md w-full h-12">
                                                     No Akaun
                                         </button>
                                     </div>
@@ -87,60 +48,10 @@ export default function Carian({props}){
                         </div>
                     </div>
                 </div>
+                <Search type={type}/>
                 <Footer />
                 </div>
                 
             </div>
             );
-    }
-    else
-    {
-        return (
-            <div>
-            <Sidebar />
-                <div className="relative md:ml-64 bg-gray-400" style={{ height: "100%" }}>
-                    <Navbar />
-                    {/* Header */}
-                        <div className="relative bg-gray-600 md:pt-32 pt-4 pb-4">
-                            <form onSubmit={(e) => handleSearch(e)}>
-                                <div className="px-4 md:px-10 mx-auto w-full">
-                                    <div className="flex flex-wrap">
-                                        <div className="w-full px-4">
-                                            <div className="relative flex flex-col min-w-0 break-words rounded mb-6">
-                                                    <div>
-                                                        <input aria-label="id" id="id" {...id} name="id" type="text" required className="mb-2 bg-white appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5" 
-                                                        placeholder= {
-                                                            search.type == 'akaun' ? ('No Akaun') 
-                                                            : search.type == 'ssm' ? ('No SSM Pendaftaran Syarikat') 
-                                                            : search.type == 'nokp' ? ('No Kad Pengenalan') 
-                                                            : ('Carian...')
-                                                        } />
-                                                    </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="flex flex-initial flex-row-reverse pt-4 pb-4">
-                                        <button id="type" type="submit" className="text-white text-center bg-green-500 flex-row-reverse rounded-full w-32 h-12">
-                                                    Add
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                <Footer/>
-            </div>
-        );
-    }
-}
-
-const useFormInput = initialValue => {
-    const [value, setValue] = useState(initialValue);
-    const handleChange = e => {
-      setValue(e.target.value);
-    }
-    return {
-      value,
-      onChange: handleChange
-    }
 }
