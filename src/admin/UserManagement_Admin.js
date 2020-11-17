ee51f80790af986e9680711d6cdd89957fc19c64
wanasyraf96 <wanasyraf96@yahoo.com>
2020-11-17 18:40:14 +0800

    commit
    User management

--- src/admin/UserManagement_Admin.js
+++ src/admin/UserManagement_Admin.js
@@ -5,6 +5,7 @@ import { DataGrid } from "@material-ui/data-grid";
 import Sidebar from "./Sidebar_Admin";
 import Navbar from "../components/Navbars/AdminNavbar";
 import Footer from "../components/Footers/Footer";
+import CardUser from "../components/Cards/CardUser";
 
 function Dashboard(props) {
   const token = getToken();
@@ -35,32 +36,108 @@ function Dashboard(props) {
     { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
   ];
 
+  // return (
+  //   <div>
+  //     <Sidebar />
+  //     <div className="relative md:ml-64 bg-blue-600" style={{ height: "100%" }}>
+  //       <Navbar />
+  //       {/* Header */}
+  //       <div className="relative bg-blue-600 md:pt-32 pb-32 pt-12">
+  //         <div className="px-4 md:px-10 mx-auto w-full">
+  //           <div className="flex flex-wrap">
+  //             Header
+  //           </div>
+  //           <div className="flex flex-wrap">
+  //             <div style={{ height: 400, width: "100%" }}>
+  //               <DataGrid
+  //                 rows={rows}
+  //                 rowHeight={104}
+  //                 columns={columns}
+  //                 pageSize={5}
+  //                 width="100%"
+  //                 checkboxSelection
+  //                 className="bg-white"
+  //               />
+  //             </div>
+  //           </div>
+  //         </div>
+  //       </div>
+  //       <div className="px-4 md:px-10 mx-auto w-full m-24"></div>
+  //       <Footer />
+  //     </div>
+  //   </div>
+  // );
+
   return (
-    <div>
+    <div className="">
       <Sidebar />
-      <div className="relative md:ml-64 bg-blue-600" style={{ height: "100%" }}>
+      <div className="relative md:ml-64 bg-blue-600" style={{height: "100%"}}>
         <Navbar />
-        {/* Header */}
-        <div className="relative bg-blue-600 md:pt-32 pb-32 pt-12">
-          <div className="px-4 md:px-10 mx-auto w-full">
-            <div className="flex flex-wrap">
-              Header
-            </div>
-            <div className="flex flex-wrap">
-              <div style={{ height: 400, width: "100%" }}>
-                <DataGrid
-                  rows={rows}
-                  columns={columns}
-                  pageSize={5}
-                  width="100%"
-                  checkboxSelection
-                  className="bg-white"
-                />
+          <div className="relative bg-blue-600 pb-32 pt-12">
+
+          <div className="px-4 md:my-4 md:px-2 mx-auto w-full">
+          <div className="flex flex-wrap">
+              <div className="w-full px-4">
+                  <div className="relative flex flex-col min-w-0 break-words bg-blue-100 border-b border-gray-400 shadow-lg">
+                      <div className="flex-auto p-4">
+                          <div className="flex flex-row border-b border-gray-600">
+                              <div className="relative w-2/12 md:w-1/12 pr-4 flex-initial">
+                                  <h5 className="uppercase font-medium text-xs text-gray-600">
+                                  {
+                                  // dataset.tempoh
+                                  // bill.description
+                                  // bill.add_harta
+                                  }Bil.
+                                  </h5>
+                              </div>
+                              <div className="relative w-4/12 lg:w-3/12 pr-4 flex-grow">
+                                      <h5 className="uppercase font-medium text-xs text-gray-600">
+                                      {
+                                      // dataset.tempoh
+                                      // bill.description
+                                      // bill.add_harta
+                                      }No Kad Pengenalan
+                                      </h5>
+                              </div>
+                              <div className="relative lg:w-6/12 pr-4 flex-grow hidden lg:block">
+                                      <h5 className="uppercase font-medium text-xs text-gray-600">
+                                      {
+                                      // dataset.tempoh
+                                      // bill.description
+                                      // bill.add_harta
+                                      }Nama
+                                      </h5>
+                              </div>
+                              <div className="relative lg:w-2/12 pr-4 flex-grow hidden lg:block">
+                                <h5 className="uppercase font-medium text-xs text-gray-600">
+                                      {
+                                      // dataset.tempoh
+                                      // bill.description
+                                      // bill.add_harta
+                                      }Telefon
+                                      </h5>
+                              </div>
+                              <div className="relative w-4/12 lg:w-2/12 pr-4 flex-initial">
+                                  <h5 className="uppercase font-medium text-xs text-gray-600">
+                                  {
+                                  // dataset.tempoh
+                                  // bill.description
+                                  // bill.add_harta
+                                  }Status
+                                  </h5>
+                              </div>
+                             <div className="relative w-1/12 w-auto pl-4 flex-initial invisible">
+                                      <i className="far fa-trash-alt" style={{color:"red"}}></i>
+                              </div>
+                          </div>
+                      </div>
+                        <CardUser />
+                  </div>
               </div>
-            </div>
           </div>
-        </div>
-        <div className="px-4 md:px-10 mx-auto w-full m-24"></div>
+      </div>
+
+          </div>
         <Footer />
       </div>
     </div>
---
+++ src/components/Cards/CardUser.js
@@ -0,0 +1,119 @@
+import React, {useState, useEffect} from "react";
+import swal from "sweetalert";
+import axios from "axios";
+
+// components
+
+// const useFormInput = (initialValue) => {
+//   const [value, setValue] = useState(initialValue);
+
+//   const handleChange = (e) => {
+//     setValue(e.target.value);
+//   };
+//   return {
+//     value,
+//     onChange: handleChange,
+//   };
+// };
+
+export default function CardUser() {
+
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [user,setUser] = useState([]);
+
+  useEffect(() =>{
+        setLoading(true);
+        axios.get('https://mymps.corrad.my/int/api_generator.php?api_name=user_list')
+      .then(res => {
+          if (res.data.status === 'success'){
+              console.log(JSON.parse(res.data.data));
+            setUser(JSON.parse(res.data.data));
+            setLoading(false);
+          }
+      });
+      
+  },[])
+  
+  const handleDelete = (e) => {
+      console.log('Delete');
+  }
+  const handleEdit = (e) => {
+      console.log('Edit');
+  }
+
+  if(loading === false){
+    let bil = 0;
+    const users = user.length ? (
+    
+        user.map(data => {
+            bil++;
+            return (
+                <div key = {data.U_USERIC}>
+                    <div className="flex-auto pb-4 px-4">
+                        <div className="flex flex-row border-b border-gray-400">
+                            <div className="relative w-2/12 md:w-1/12 pr-4">
+                                <span className="font-semibold uppercase text-sm text-gray-700">
+                                {bil}
+                                </span>
+                            </div>
+                            <div className="relative w-4/12 lg:w-3/12 pr-4 flex-grow">
+                                    <span className="font-semibold uppercase text-sm text-gray-700">
+                                    {
+                                        data.U_USERIC
+                                    }
+                                    </span>
+                            </div>
+                            <div className="relative lg:w-6/12 pr-4 flex-grow hidden lg:block">
+                                    <span className="font-semibold uppercase text-sm md:text-sm  text-gray-700">
+                                    {
+                                        data.U_USERNAME
+
+                                    }
+                                    </span>
+                            </div>
+                            <div className="relative lg:w-2/12 pr-4 flex-grow hidden lg:block">
+                                    <span className="font-semibold uppercase text-sm text-gray-700">
+                                    {
+                                        data.U_USERPHONE
+
+                                    }
+                                    </span>
+                            </div>
+                            <div className="relative w-4/12 lg:w-2/12 pr-4 flex-initial">
+                                <span className="font-semibold uppercase text-xs text-green-700">
+                                {
+                                    data.U_USERSTATUS
+
+                                }
+                                </span>
+                            </div>
+                            <div className="relative w-1/12 w-auto pl-4 flex-initial">
+                                <button onClick={(e) => handleEdit(e)}>
+                                    <i className="fas fa-edit" style={{color:"green"}}></i>
+                                </button>
+                                <button onClick={(e) => handleDelete(e)}>
+                                    <i className="far fa-trash-alt" style={{color:"red"}}></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+        })
+    ) : (<div></div>);
+        return (
+            <div>
+                {users}
+            </div>
+        );
+    }
+    else{
+        return (
+            <>
+                Loading...
+            </>
+        );
+    }
+}
+
