0b8e9c36cb6f30b83d5695c93c05eaf7b00a2936
wanasyraf96 <wanasyraf96@yahoo.com>
2020-11-27 14:01:06 +0800

    Update Sidebar_Admin.js

--- src/admin/Sidebar_Admin.js
+++ src/admin/Sidebar_Admin.js
@@ -93,27 +93,27 @@ export default function Sidebar() {
 
             <ul className="md:flex-col md:min-w-full flex flex-col list-none">
             {              
-              <li className="items-center">
-                <Link
-                  className={
-                    "text-xs uppercase py-3 font-bold block " +
-                    (window.location.href.indexOf("/admin/dashboard") !== -1
-                      ? "text-blue-500 hover:text-blue-600"
-                      : "text-gray-800 hover:text-gray-600")
-                  }
-                  to="/admin/dashboard"
-                >
-                  <i
-                    className={
-                      "fas fa-tv mr-2 text-sm " +
-                      (window.location.href.indexOf("/admin/dashboard") !== -1
-                        ? "opacity-75"
-                        : "text-gray-400")
-                    }
-                  ></i>{" "}
-                  Utama
-                </Link>
-              </li>
+              // <li className="items-center">
+              //   <Link
+              //     className={
+              //       "text-xs uppercase py-3 font-bold block " +
+              //       (window.location.href.indexOf("/admin/dashboard") !== -1
+              //         ? "text-blue-500 hover:text-blue-600"
+              //         : "text-gray-800 hover:text-gray-600")
+              //     }
+              //     to="/admin/dashboard"
+              //   >
+              //     <i
+              //       className={
+              //         "fas fa-tv mr-2 text-sm " +
+              //         (window.location.href.indexOf("/admin/dashboard") !== -1
+              //           ? "opacity-75"
+              //           : "text-gray-400")
+              //       }
+              //     ></i>{" "}
+              //     Utama
+              //   </Link>
+              // </li>
             }
 
               <li className="items-center">
