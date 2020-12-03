cf02dc8b76a65145f4d1ba294b70f5b71e62fd20
SyahrilSulaiman <mohdsyahrilbinsulaiman@gmail.com>
2020-12-03 10:50:15 +0800

    commit on master

--- src/Register.js
+++ src/Register.js
@@ -175,7 +175,7 @@ function Register(props) {
             if (result.status == "success") {
 
               setLoading("false");
-              swal("Tahniah!", "Pendaftaran sebagai pengguna MyMPS berjaya! Sila sahkan akaun anda sebagai pengguna melalui klik pada pautan di emel anda.", "success");
+              //swal("Tahniah!", "Pendaftaran sebagai pengguna MyMPS berjaya! Sila sahkan akaun anda sebagai pengguna melalui klik pada pautan di emel anda.", "success");
 
               setUserSession(btoa(formdata), username.value, ssm.value, email.value);
               sessionStorage.setItem("notel", notel.value);
