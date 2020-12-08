9c2da0feb8fbe8624bb69a484740ed2a686e4faf
wanasyraf96 <wanasyraf96@yahoo.com>
2020-12-08 17:00:59 +0800

    Update MultiAccountPayment.js

--- src/MultiAccountPayment.js
+++ src/MultiAccountPayment.js
@@ -42,7 +42,7 @@ function Pay() {
 
         let total = 0;
         for(var i = 0; i < account.length; i++){
-            setAmount(total = total + account[i]["BAKI_TUNGGAK"]);
+            setAmount(total = total + account[i]["BAKI_DAHULU"] + account[i]["CAJ_DIKENAKAN"] + account[i]["CUKAI_SEMASA"] + account[i]["TMP_LAIN"] + account[i]["TUNGGAKAN_SEMASA"] + account[i]["WARAN_TAHANAN"] );
         }
 
         fetch('https://toyyibpay.com/api/getBankFPX')
